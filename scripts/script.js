7e66f8934bbdb6b88fbfc8c2113f42f17155f6be
Leon Hsu <leonhsu95@gmail.com>
2021-03-08 18:05:32 +1100

    Responsive Sticky Navbar added

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ function navSlide() {
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
     
+    
     burger.addEventListener("click", () => {
         //Toggle Nav
         nav.classList.toggle("nav-active");
@@ -11,11 +12,12 @@ function navSlide() {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ""
-            } else {
+            } 
+            else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
-        
+
         //Burger Animation
         burger.classList.toggle("toggle");
     });
@@ -23,3 +25,23 @@ function navSlide() {
 }
 
 navSlide();
+
+// When the user scrolls the page, execute Function
+window.onscroll = function() {stickyNav()};
+
+// Get the navbar
+const navbar = document.getElementById("header");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+stickyNav();
